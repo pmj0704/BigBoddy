a38143a357c27ac8956a1491f8d09fbf6183ac63
Minjoon Park <77655326+pmj0704@users.noreply.github.com>
2023-07-05 09:54:21 +0900

    FirstCommit

---
+++ Source/BoomBangPow/BoomBangPowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BoomBangPowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOOMBANGPOW_API ABoomBangPowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
