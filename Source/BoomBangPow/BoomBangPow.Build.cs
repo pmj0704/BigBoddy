711cb182aaa692f59e1d196f1a5455e958cbaf54
Minjoon Park <77655326+pmj0704@users.noreply.github.com>
2023-09-01 09:56:20 +0900

    BuildSuccess

--- Source/BoomBangPow/BoomBangPow.Build.cs
+++ Source/BoomBangPow/BoomBangPow.Build.cs
@@ -8,14 +8,8 @@ public class BoomBangPow : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "EnhancedInput", "UMG", "AIModule", "GameplayTasks", "NavigationSystem", "PublicDependencyModuleNames", "Slate", "SlateCore", "OnlineSubsystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" , "EnhancedInput", "UMG", "AIModule", "GameplayTasks", "NavigationSystem"});
 
-		// Uncomment if you are using Slate UI
-		//PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "OnlineSubsystem" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 	}
 }
